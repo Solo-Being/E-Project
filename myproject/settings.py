6a4af492072e3035366f2d0a217d52da0bfd4598
Adith Raj <rajadith868@gmail.com>
2026-07-26 14:59:53 +0530

    Remove duplicate settings from merge conflict

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,13 +32,6 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['chronex-cfut.onrender.com', '127.0.0.1', 'localhost']
 SECRET_KEY = 'django-insecure-aruh3!b&t(mw6rch)43cnr+mhzv%jp5a4h^ki6=)zuuq0s6tbp'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-
-ALLOWED_HOSTS = []
-
-
 
 # Application definition
 
